9c9eec1272ed9905904565c9f0448140f94402bc
OkadaIchirou <itr121416@me.com>
2017-02-28 11:51:08 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,19 @@
 class MessagesController < ApplicationController
+    include ActiveModel::Conversion
   def index
+    @message = Message.new
+    @messages = Message.all
   end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+   def persisted? ; false ; end
 end
